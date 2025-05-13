dcd7d17b36246c1ed4b181d0cbef3a4b759846cf
Adithya <aadhii.yz@gmail.com>
2025-05-13 10:43:36 +0530

    Enabled raw mode. Turned off ECHO.

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
+#include <termios.h>
 #include <unistd.h>
+
+void enableRawMode() {
+  struct termios raw;
+  tcgetattr(STDIN_FILENO, &raw);
+
+  raw.c_lflag &= ~(ECHO);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(int argc, char *argv[])
 {
+  enableRawMode();
+
   char ch;
   while (read(STDIN_FILENO, &ch, 1) == 1 && ch != 'q');
   return 0;
