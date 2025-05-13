81e0d605e061e18f76abb8ff7e67a1dedf08c20f
Adithya <aadhii.yz@gmail.com>
2025-05-13 14:44:20 +0530

    Cleared screen at required places.

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 // Headers :
-#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,6 +13,9 @@ struct termios inital_termios;
 
 // Terminal :
 void die(const char* s) {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
   perror(s);
   exit(1);
 }
@@ -52,12 +54,20 @@ char editorReadKey() {
   return ch;
 }
 
+// Output :
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 // Input :
 void editorProcessKeypress() {
   char ch = editorReadKey();
 
   switch (ch) {
     case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
   }
@@ -69,6 +79,7 @@ int main(int argc, char *argv[])
   enableRawMode();
 
   while (1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
   return 0;
