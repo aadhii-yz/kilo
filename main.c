80d1b922a70edd745c6746a83b976a639a3cb023
Adithya <aadhii.yz@gmail.com>
2025-05-13 18:06:36 +0530

    Added a Welcome message.

--- main.c
+++ main.c
@@ -8,6 +8,8 @@
 #include <unistd.h>
 
 // Defines :
+#define KILO_VERSION "0.0.1"
+
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 // Data :
@@ -120,13 +122,22 @@ void abFree(struct abuf* ab) {
 void editorDrawRows(struct abuf* ab) {
   int y;
   for (y = 0; y < E.screenRows; y++) {
-    abAppend(ab, "~", 1);
+
+    if (y == E.screenRows / 3) {
+      char welcome[80];
+      int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+      if (welcomelen > E.screenCols)
+        welcomelen = E.screenCols;
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
+
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenRows - 1) {
       abAppend(ab, "\r\n", 2);
     }
   }
-  abAppend(ab, "~", 1);
 }
 
 void editorRefreshScreen() {
