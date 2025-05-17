8e5cb944d996a13cfbbab1a9238682c865b5c6b5
Adithya <aadhii.yz@gmail.com>
2025-05-17 17:20:08 +0530

    Limiting horizontal and vertical scrolling.

--- main.c
+++ main.c
@@ -291,6 +291,8 @@ void editorRefreshScreen() {
 
 // Input :
 void editorMoveCursor(int key) {
+  erow* row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -298,7 +300,9 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
-      E.cx++;
+      if (row && E.cx < row->size) {
+        E.cx++;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
@@ -310,6 +314,12 @@ void editorMoveCursor(int key) {
         E.cy++;
       }
       break;
+  }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
   }
 }
 
