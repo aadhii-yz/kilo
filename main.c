eb1c29b86bab59a605acce457e357d88cb24918a
Adithya <aadhii.yz@gmail.com>
2025-05-19 11:24:23 +0530

    Enable text insertion in view.

--- main.c
+++ main.c
@@ -228,6 +228,23 @@ void editorAppendRow(char* s, size_t len) {
   E.numrows++;
 }
 
+void editorRowInsertChar(erow* row, int at, int ch) {
+  if (at < 0 || at > row->size) at = row->size;
+  row->chars = realloc(row->chars, row->size + 2);
+  memmove(&row->chars[at + 1], &row->chars[at], row->size - at + 1);
+  row->size++;
+  row->chars[at] = ch;
+  editorUpdateRow(row);
+}
+
+// Editor Operations :
+void editorInsertChar(int ch) {
+  if (E.cy == E.numrows) {
+    editorAppendRow("", 0);
+  }
+  editorRowInsertChar(&E.row[E.cy], E.cx, ch);
+}
+
 // File I/O :
 void editorOpen(char* filename) {
   free(E.filename);
@@ -464,6 +481,10 @@ void editorProcessKeypress() {
     case ARROW_LEFT:
     case ARROW_RIGHT:
       editorMoveCursor(ch);
+      break;
+
+    default:
+      editorInsertChar(ch);
       break;
   }
 }
