a776935ab17c6bbe67f39f34d1112f8879023117
Adithya <aadhii.yz@gmail.com>
2025-05-23 16:02:14 +0530

    Handle saving properly.

--- main.c
+++ main.c
@@ -59,6 +59,9 @@ struct editorConfig {
 
 struct editorConfig E;
 
+// Prototypes :
+void editorSetStatusMessage(const char* fmt, ...);
+
 // Terminal :
 void die(const char* s) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
@@ -295,10 +298,19 @@ void editorSave() {
   char* buf = editorRowsToString(&len);
 
   int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
-  ftruncate(fd, len);
-  write(fd, buf, len);
-  close(fd);
+  if (fd != -1) {
+    if (ftruncate(fd, len) != -1) {
+      if (write(fd, buf, len) == len) {
+        close(fd);
+        free(buf);
+        editorSetStatusMessage("%d bytes written to disk", len);
+        return;
+      }
+    }
+    close(fd);
+  }
   free(buf);
+  editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
 // Append buffer :
@@ -568,7 +580,7 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
 
-  editorSetStatusMessage("HELP: CTRL-Q = quit");
+  editorSetStatusMessage("HELP: CTRL-S = save | CTRL-Q = quit");
 
   while (1) {
     editorRefreshScreen();
